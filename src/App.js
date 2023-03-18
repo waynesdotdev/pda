133bd14a9cd22cb2b7af5f4c569fad4746b7ebb5
Wayne Fox <91109871+foxwayne1@users.noreply.github.com>
2023-03-18 13:30:00 +0000

    addinga nd deisplays props through new file

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ function App() {
       <header>
         <h1>Personal Digital Assistants</h1>
       </header>
-      <ProfileCard />
-      <ProfileCard />
-      <ProfileCard />
+      <ProfileCard title='Alexa' handle='@alexa99' />
+      <ProfileCard title='Cortana' handle='@cortana29' />
+      <ProfileCard title='Siri' handle='@siri99' />
     </div>
   )
 }
--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -1,7 +1,12 @@
 import React from 'react'
 
-const ProfileCard = () => {
-  return <div>ProfileCard</div>
+const ProfileCard = props => {
+  return (
+    <div>
+      <h1>{props.title}</h1>
+      <h2>{props.handle}</h2>
+    </div>
+  )
 }
 
 export default ProfileCard
