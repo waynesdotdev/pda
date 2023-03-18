93f4ca7282984ecfda4fa5169399ad0f42cdabf1
Wayne Fox <91109871+foxwayne1@users.noreply.github.com>
2023-03-18 13:45:01 +0000

    images added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import ProfileCard from './ProfileCard'
+import alexa from './images/alexa.png'
+import cortana from './images/cortana.png'
+import siri from './images/siri.png'
 
 function App() {
   return (
@@ -6,9 +9,9 @@ function App() {
       <header style={{ backgroundColor: 'violet', padding: '20px' }}>
         <h1>Personal Digital Assistants</h1>
       </header>
-      <ProfileCard title='Alexa' handle='@alexa99' />
-      <ProfileCard title='Cortana' handle='@cortana32' />
-      <ProfileCard title='Siri' handle='@siri01' />
+      <ProfileCard title='Alexa' handle='@alexa99' image={alexa} />
+      <ProfileCard title='Cortana' handle='@cortana32' image={cortana} />
+      <ProfileCard title='Siri' handle='@siri01' image={siri} />
     </div>
   )
 }
--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -1,6 +1,7 @@
-const ProfileCard = ({ title, handle }) => {
+const ProfileCard = ({ title, handle, image }) => {
   return (
     <div>
+      <img src={image} alt={title} />
       <h1>The Title is {title}</h1>
       <h2>The handle is {handle}</h2>
     </div>
