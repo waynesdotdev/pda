d53e943a0baf8c9fe250541dcc6ca6d0978aa54d
Wayne Fox <91109871+foxwayne1@users.noreply.github.com>
2023-03-18 13:24:14 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import ProfileCard from './ProfileCard'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <h1>Personal Digital Assistants</h1>
       </header>
+      <ProfileCard />
+      <ProfileCard />
+      <ProfileCard />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
