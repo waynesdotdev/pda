75385fdea9c3d476448dab3dd88a3d2b09076743
Wayne Fox <91109871+foxwayne1@users.noreply.github.com>
2023-03-18 14:04:15 +0000

    added container to fix css issues

--- src/App.js
+++ src/App.js
@@ -17,9 +17,26 @@ function App() {
       >
         <h1>Personal Digital Assistants</h1>
       </header>
-      <ProfileCard title='Alexa' handle='@alexa99' image={alexa} />
-      <ProfileCard title='Cortana' handle='@cortana32' image={cortana} />
-      <ProfileCard title='Siri' handle='@siri01' image={siri} />
+
+      <div className='container'>
+        <section className='section'>
+          <div className='columns'>
+            <div className='column is-4'>
+              <ProfileCard title='Alexa' handle='@alexa99' image={alexa} />
+            </div>
+            <div className='column is-4'>
+              <ProfileCard
+                title='Cortana'
+                handle='@cortana32'
+                image={cortana}
+              />
+            </div>
+            <div className='column is-4'>
+              <ProfileCard title='Siri' handle='@siri01' image={siri} />
+            </div>
+          </div>
+        </section>
+      </div>
     </div>
   )
 }
